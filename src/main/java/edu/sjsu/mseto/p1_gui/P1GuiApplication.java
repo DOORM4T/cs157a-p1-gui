534f58d5bec5c7154b9fccc7006cf88e512129aa
Matthew Seto <46092255+DOORM4T@users.noreply.github.com>
2021-11-14 12:37:28 -0800

    Initial commit

---
+++ src/main/java/edu/sjsu/mseto/p1_gui/P1GuiApplication.java
@@ -0,0 +1,16 @@
+package edu.sjsu.mseto.p1_gui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class P1GuiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(P1GuiApplication.class, args);
+    }
+}
---
+++ src/test/java/edu/sjsu/mseto/p1_gui/P1GuiApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.sjsu.mseto.p1_gui;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class P1GuiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
